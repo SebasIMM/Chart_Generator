45b62aaa4dd0030e2c5d0e64c576c773a9b3c695
Sebastian Medel <sevastian.ignis@gmail.com>
2024-01-15 16:06:09 -0300

    feat: JS created and finalized

---
+++ script.js
@@ -0,0 +1,93 @@
+let chartData = {
+    labels: [],
+    datasets: [{
+        label: '# of Values',
+        data: [],
+        backgroundColor: [
+            'grba(255, 99, 132, 0.5)',
+            'grba(54, 162, 235, 0.5)',
+            'grba(255, 206, 86, 0.5)',
+            'grba(75, 192, 192, 0.5)',
+            'grba(153, 102, 255, 0.5)',
+            'grba(255, 159, 64, 0.5)'
+        ],
+        borderColor: [
+            'grba(255, 99, 132, 1)',
+            'grba(54, 162, 235, 1)',
+            'grba(255, 206, 86, 1)',
+            'grba(75, 192, 192, 1)',
+            'grba(153, 102, 255, 1)',
+            'grba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+
+function createChart(type, height = 400) {
+    const canvasContainer = document.getElementById('canvas-container');
+    
+    canvasContainer.innerHTML = `<canvas id="myChart"></canvas>`;
+    canvasContainer.style.height = `${height}px`;
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    return new Chart(ctx, {
+        type: type,
+        data: chartData,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            onClick: (event, activeElements) => {
+                if (activeElements.length > 0) {
+                    const {
+                        datasetIndex,
+                        index
+                    } =
+                        activeElements[0];
+                    removeData(datasetIndex, index)
+                }
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            hover: {
+                mode: 'index',
+                intersect: false
+            }
+        }
+    });
+}
+
+let myChart = createChart('bar')
+
+function addData() {
+    const labelInput = document.getElementById('labelInput');
+    const dataInput = document.getElementById('dataInput');
+
+    if (labelInput.value && dataInput.value) {
+        chartData.labels.push(labelInput.value)
+        chartData.datasets.forEach((dataset) => {
+            dataset.data.push(dataInput.value)
+        })
+        myChart.update()
+        labelInput.value = '';
+        dataInput.value = '';
+    }
+}
+
+function updateChartType() {
+    const selectedType = document.getElementById('chartType').value;
+    myChart.destroy();
+    myChart = createChart(selectedType);
+}
+
+function removeData(datasetIndex, index) {
+    if (chartData.labels.length > index) {
+        chartData.labels.splice(index, 1)
+        chartData.datasets[datasetIndex].data.splice(index, 1)
+        myChart.update();
+    }
+}
